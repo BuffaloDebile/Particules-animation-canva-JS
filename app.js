ac890fa79f0f62ec4551ec0ba3e7816bea6f064b
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-08-08 22:19:12 +0200

    animating the particules with movement

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Particle {
-  constructor(x, y, directionX, directionY, color) {
+  constructor(x, y, directionX, directionY, size, color) {
     this.x = x;
     this.y = y;
     this.directionY = directionY;
     this.directionX = directionX;
+    this.size = size;
     this.color = color;
   }
-
   draw() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fillStyle = this.color;
     ctx.fill();
   }
+
   update() {
     if (this.x > canvas.width || this.x < 0) {
       this.directionX = -this.directionX;
@@ -30,37 +31,52 @@ class Particle {
     this.draw();
   }
 }
-
-const particle1 = new Particle(10, 10, 50, 55, 2, '#f1f1f1f');
+const particle1 = new Particle(10, 10, 50, 55, 2, '#f1f1f1');
+console.log(particle1);
 
 let particlesArray;
 
 function init() {
   particlesArray = [];
-  const numberOfParticles = (canvas.height * canvas.height) / 7000;
+
+  const numberOfParticles = (canvas.height * canvas.width) / 9000;
 
   for (let i = 0; i < numberOfParticles; i++) {
+    // [1,3[
     const size = Math.random() * 2 + 1;
+    // return Math.random() * (max - min + 1) + min;
     const x = Math.random() * (innerWidth - 10 - 10 + 1) + 10;
     const y = Math.random() * (innerHeight - 10 - 10 + 1) + 10;
 
     const directionX = cleanDirection();
     const directionY = cleanDirection();
-    particlesArray.push(new Particle(x, y, directionX, directionY, '#f1f1f1'));
+    particlesArray.push(
+      new Particle(x, y, directionX, directionY, size, '#f1f1f1'),
+    );
   }
 }
+
 init();
 
 function cleanDirection() {
-  // 0 or 1
+  // 0 ou 1
   const random = Math.trunc(Math.random() * 2);
   if (random) {
-    // de 0.5 a 1.5 non included, non trunc
+    // de 0.5 à 1.5 non inclu, non trunc [0.5,1.5[
     return Math.random() * 1 + 0.5;
   } else {
-    // de -0.5 a -1.5 non included, non trunc
-    return Math.random() * -1 + -0.5;
+    // de -0.5 à -1.5 non inclu, non trunc ]-1.5,-0.5]
+    return Math.random() * -1 - 0.5;
   }
 }
 
+function animate() {
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
 
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+  }
+
+  requestAnimationFrame(animate);
+}
+animate();
