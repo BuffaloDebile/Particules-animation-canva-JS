8b84c8980184eaad155f441b08b08f4e0caa6e9f
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-08-08 21:57:18 +0200

    creating particles

--- app.js
+++ app.js
@@ -32,3 +32,35 @@ class Particle {
 }
 
 const particle1 = new Particle(10, 10, 50, 55, 2, '#f1f1f1f');
+
+let particlesArray;
+
+function init() {
+  particlesArray = [];
+  const numberOfParticles = (canvas.height * canvas.height) / 7000;
+
+  for (let i = 0; i < numberOfParticles; i++) {
+    const size = Math.random() * 2 + 1;
+    const x = Math.random() * (innerWidth - 10 - 10 + 1) + 10;
+    const y = Math.random() * (innerHeight - 10 - 10 + 1) + 10;
+
+    const directionX = cleanDirection();
+    const directionY = cleanDirection();
+    particlesArray.push(new Particle(x, y, directionX, directionY, '#f1f1f1'));
+  }
+}
+init();
+
+function cleanDirection() {
+  // 0 or 1
+  const random = Math.trunc(Math.random() * 2);
+  if (random) {
+    // de 0.5 a 1.5 non included, non trunc
+    return Math.random() * 1 + 0.5;
+  } else {
+    // de -0.5 a -1.5 non included, non trunc
+    return Math.random() * -1 + -0.5;
+  }
+}
+
+
